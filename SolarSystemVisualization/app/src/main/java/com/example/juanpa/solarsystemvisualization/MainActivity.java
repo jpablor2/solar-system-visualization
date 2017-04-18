8a85a252a84dc8d2b014ffb87a999babf88b2a6f
Juan Pablo Rodriguez Rojas <jpablor2@hotmail.com>
2017-04-18 11:56:40 -0600

    Imagenes y codigo para insertarlas en el mainactivity

--- SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/MainActivity.java
+++ SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.juanpa.solarsystemvisualization;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayout1 = (LinearLayout) findViewById(R.id.linearLayout1);
+        for(int x=0;x<3;x++) {
+            ImageView image = new ImageView(MainActivity.this);
+            image.setBackgroundResource(R.drawable.images1);
+            linearLayout1.addView(image);
+        }
     }
 }
