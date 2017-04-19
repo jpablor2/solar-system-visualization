e6bd36226fc4f15ac1e5bc19cf99d583cc80d7af
Juan Pablo Rodriguez Rojas <jpablor2@hotmail.com>
2017-04-19 00:12:24 -0600

    estructura terminada

--- SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/ResultadoActivity.java
+++ SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/ResultadoActivity.java
@@ -1,7 +1,9 @@
 package com.example.juanpa.solarsystemvisualization;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ResultadoActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ResultadoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+    }
+
+    public void login(View view){
+        Intent login_intent = new Intent(this, InfoActivity.class);
+        startActivity(login_intent);
     }
 }
