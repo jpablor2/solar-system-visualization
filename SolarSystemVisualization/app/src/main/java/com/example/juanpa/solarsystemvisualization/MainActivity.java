cca9658a45ebd18852ff0952e74fdd14cdc78dd5
Juan Pablo Rodriguez Rojas <jpablor2@hotmail.com>
2017-04-18 21:33:45 -0600

    navigation bar

--- SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/MainActivity.java
+++ SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.juanpa.solarsystemvisualization;
 
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,13 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout linearLayout1 = (LinearLayout) findViewById(R.id.linearLayout1);
+        /*LinearLayout linearLayout1 = (LinearLayout) findViewById(R.id.linearLayout1);
         for(int x=0;x<1;x++) {
             ImageView image = new ImageView(MainActivity.this);
 
             image.setBackgroundResource(R.drawable.images1);
 
             linearLayout1.addView(image);
-        }
+        }*/
+    }
+
+    public void login(View view){
+        Intent login_intent = new Intent(this, HomeActivity.class);
+        startActivity(login_intent);
     }
 }
