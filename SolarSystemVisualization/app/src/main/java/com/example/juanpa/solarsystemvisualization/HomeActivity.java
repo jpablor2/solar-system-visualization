87210270385492e6cbb3492e229b50086001fa79
Juan Pablo Rodriguez Rojas <jpablor2@hotmail.com>
2017-04-18 22:53:26 -0600

    solucion de problema con el gradle

--- SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/HomeActivity.java
+++ SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/HomeActivity.java
@@ -11,15 +11,14 @@ public class HomeActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
-    private Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mToolbar=(Toolbar)findViewById(R.id.nav_action);
-        setSupportActionBar(mToolbar);
+
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
