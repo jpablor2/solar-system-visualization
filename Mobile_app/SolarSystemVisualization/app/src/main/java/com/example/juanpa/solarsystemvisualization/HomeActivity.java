5b7e590d04e5a356d631e6e2dd44343880e09b03
byga1402 <gabrielrojas_a@hotmail.com>
2017-05-10 01:14:50 -0600

    Sincronizada librería de análisis de QR

--- Mobile_app/SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/HomeActivity.java
+++ Mobile_app/SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/HomeActivity.java
@@ -1,6 +1,11 @@
 package com.example.juanpa.solarsystemvisualization;
 
+import android.Manifest;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +13,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,7 +26,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
+        getPermissionToUseCamera();
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
@@ -30,18 +36,80 @@ public class HomeActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+
+    private static final int USE_CAMERA_PERMISSIONS_REQUEST = 1;
+
+    public void getPermissionToUseCamera() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.CAMERA)) {
+
+            }
+
+            requestPermissions(new String[]{Manifest.permission.CAMERA},
+                    USE_CAMERA_PERMISSIONS_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == USE_CAMERA_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Use camera permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+
+                boolean showRationale = shouldShowRequestPermissionRationale(Manifest.permission.CAMERA);
+
+                if (showRationale) {
+                    // do something here to handle degraded mode
+                } else {
+                    Toast.makeText(this, "Use camera permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(mToggle.onOptionsItemSelected(item)){
             return true;
-
         }
-
         return super.onOptionsItemSelected(item);
     }
 
+
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+
     public void login(View view){
-        Intent login_intent = new Intent(this, ResultadoActivity.class);
-        startActivity(login_intent);
+        try {
+            //start the scanning activity from the com.google.zxing.client.android.SCAN intent
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException anfe) {
+            //on catch, show the download dialog
+            //showDialog(MainActivity.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                //get the extras that are returned from the intent
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
+                toast.show();
+                Intent login_intent = new Intent(this, ResultadoActivity.class);
+                startActivity(login_intent);
+            }
+        }
     }
 }
