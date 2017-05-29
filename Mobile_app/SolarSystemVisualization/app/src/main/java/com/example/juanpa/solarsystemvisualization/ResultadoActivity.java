3241e3612e5fde41be6b519e3f445db81e879059
byga1402 <gabrielrojas_a@hotmail.com>
2017-05-28 19:41:59 -0600

    Agregada libreria con las actividades de Unity

--- Mobile_app/SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/ResultadoActivity.java
+++ Mobile_app/SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/ResultadoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import com.byga1402.solar_panel.*;
 
 public class ResultadoActivity extends AppCompatActivity {
 
@@ -15,7 +16,14 @@ public class ResultadoActivity extends AppCompatActivity {
     }
 
     public void login(View view){
-        Intent login_intent = new Intent(this, InfoActivity.class);
-        startActivity(login_intent);
+        //Intent login_intent = new Intent(this, InfoActivity.class);
+        //startActivity(login_intent);
+
+        Intent intent = new Intent(this, UnityPlayerActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("Left","Left Text");
+        bundle.putString("Right","Right Text");
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
