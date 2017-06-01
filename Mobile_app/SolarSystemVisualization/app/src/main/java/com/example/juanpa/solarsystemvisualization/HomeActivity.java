0ab9f4d4a4157834075f218ba759b784dc11e60f
byga1402 <gabrielrojas_a@hotmail.com>
2017-06-01 07:43:16 -0600

    Agregado layout de formularios

--- Mobile_app/SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/HomeActivity.java
+++ Mobile_app/SolarSystemVisualization/app/src/main/java/com/example/juanpa/solarsystemvisualization/HomeActivity.java
@@ -120,6 +120,7 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
                 //get the extras that are returned from the intent
@@ -129,8 +130,6 @@ public class HomeActivity extends AppCompatActivity {
                 //toast.show();
 
                 APIClient APIClient = new APIClient();
-
-                final Bundle bundle = new Bundle();
 
                 MyApiEndpointInterface apiService = APIClient.getClient().create(MyApiEndpointInterface.class);
                 String id_arreglo=contents;
@@ -144,15 +143,21 @@ public class HomeActivity extends AppCompatActivity {
                         /*String statusCode = String.valueOf(response.code());
                         List<Arrays> arreglos= (List<Arrays>) response.body();*/
                         if(response.body()!=null) {
+                            Bundle bundle = new Bundle();
+
                             String tipo = response.body().getTipoConexion();
                             String nPaneles = response.body().getNPaneles();
                             String orientacion = response.body().getAnguloOrientacion();
                             String inclinacion = response.body().getAnguloInclinacion();
-                            bundle.putString("tipo","Tipo: "+tipo+"\n");
+                            bundle.putString("tipo","Tipo: "+ tipo +"\n");
                             bundle.putString("nPaneles","N. Paneles: "+nPaneles+"\n");
                             bundle.putString("orientacion","Orientación: "+orientacion+"\n");
                             bundle.putString("inclinacion","Inclinación: "+inclinacion+"\n");
-                            Toast.makeText(getApplicationContext(),"Response: "+ nPaneles, Toast.LENGTH_LONG).show();
+                            //Toast.makeText(getApplicationContext(),"Response: "+ tipo, Toast.LENGTH_LONG).show();
+
+                            Intent login_intent = new Intent(HomeActivity.this, ResultadoActivity.class);
+                            login_intent.putExtras(bundle);
+                            startActivity(login_intent);
                         }
 
                         else{
@@ -167,9 +172,6 @@ public class HomeActivity extends AppCompatActivity {
                 });
                 //call
 
-                Intent login_intent = new Intent(this, ResultadoActivity.class);
-                login_intent.putExtras(bundle);
-                startActivity(login_intent);
             }
         }
     }
