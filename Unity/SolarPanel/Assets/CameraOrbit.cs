b25d9c546b660f5b86c6b75f215d4607f92ef547
byga1402 <gabrielrojas_a@hotmail.com>
2017-06-21 00:58:39 -0600

    Unity project

---
+++ Unity/SolarPanel/Assets/CameraOrbit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
+            Vector2 lastFramePos = touch.position - touch.deltaPosition;
+            Vector2 firstVector = lastFramePos - screenCenter;
+            Vector2 convertedMouseInput = new Vector2(touch.position.x, touch.position.y);
+            Vector2 secondVector = convertedMouseInput - screenCenter;
+            float angle = Vector2.Angle(firstVector, secondVector);
+            Vector3 cross = Vector3.Cross(firstVector, secondVector);
+            if (cross.z < 0)
+            {
+                angle = -angle;
+            }
+            var rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + angle, 0);
+            transform.rotation = rotation;
+        }
+    }
+}
---
+++ Unity/SolarPanel/Assets/SetTexts.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetTexts : MonoBehaviour {
+    private Text LeftText;
+    private Text RightText;
+    private string left;
+    private string right;
+    // Use this for initialization
+    void Start () {
+        LeftText = GameObject.Find("LeftText").GetComponent<Text>();
+        RightText = GameObject.Find("RightText").GetComponent<Text>();
+        left = "";
+        right = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) { Application.Quit(); }
+        LeftText.text = left;
+        RightText.text = right;
+    }
+
+    public void SetLeftText(string data)
+    {
+        left = data;
+    }
+
+    public void SetRightText(string data)
+    {
+        right = data;
+    }
+}
